56d74d11ca9ae87840043961b297e5610c24ec8a
Miguel Fernandes <me@mdmfernandes.com>
2023-09-06 19:18:58 +0100

    nvim: add markdown-preview plugin

---
+++ nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,14 @@
+-- https://github.com/iamcco/markdown-preview.nvim
+return {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    lazy = true,
+    build = function() vim.fn["mkdp#util#install"]() end,
+    config = function()
+        local map = require("utils").map
+
+        map("n", "<Leader>mp", "<Cmd>MarkdownPreview<CR>")
+        map("n", "<Leader>ms", "<Cmd>MarkdownPreviewStop<CR>")
+        map("n", "<Leader>mt", "<Cmd>MarkdownPreviewToggle<CR>")
+    end
+}
