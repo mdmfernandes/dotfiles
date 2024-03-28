01017351ff9ff129004f40178b284b1099c09afe
Miguel Fernandes <me@mdmfernandes.com>
2024-03-28 21:09:33 +0000

    nvim: improve BitBake .inc files detection
    Some BitBake .inc files were not being properly detected because the
    common patterns were not used. This commit adds detection of patterns
    ":append =" and ":remove =".

--- nvim/lua/core/filetype.lua
+++ nvim/lua/core/filetype.lua
@@ -6,14 +6,28 @@ vim.filetype.add({
         service = "systemd",
         socket = "systemd",
         target = "systemd",
-        bb = "config",
         log = "log",
         _log = "log",
         LOG = "log",
-        _LOG = "log"
+        _LOG = "log",
     },
     filename = {
         ["docker-compose.yml"] = "yaml.docker-compose",
         ["docker-compose.yaml"] = "yaml.docker-compose"
+    },
+    pattern = {
+        -- Improve detection of *.inc files
+        [".*%.inc"] = {
+            priority = vim.g.filetype_inc,
+            function(path, bufnr)
+                local content = vim.api.nvim_buf_get_lines(bufnr, 0, 50, false)
+                for _, line in ipairs(content) do
+                    --  Detect BitBake files by the presence of ".*:append =" or ".*:remove ="
+                    if line:match(":append =") or line:match(":remove =") then
+                        return "bitbake"
+                    end
+                end
+            end
+        }
     }
 })
