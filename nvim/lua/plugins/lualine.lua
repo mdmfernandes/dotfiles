2c24acd7bef9089b77fc4d5c44ec1ea714b078d2
Miguel Fernandes <me@mdmfernandes.com>
2024-09-21 00:53:51 +0100

    nvim: remove nvim-navic plugin

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,15 +3,8 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
         { "nvim-tree/nvim-web-devicons" },
-        "SmiteshP/nvim-navic",
     },
     config = function()
-        local navic = require("nvim-navic")
-
-        navic.setup({
-            highlight = true
-        })
-
         require("lualine").setup({
             options = {
                 theme = "catppuccin",
@@ -19,12 +12,7 @@ return {
             sections = {
                 lualine_c = {
                     {
-                        function()
-                            return navic.get_location()
-                        end,
-                        cond = function()
-                            return navic.is_available()
-                        end
+                        "filename", path = 1,
                     },
                 },
                 lualine_z = {
