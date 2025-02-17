2e3191fb2ed30b9b5f2e4a05aa602030b80155df
Miguel Fernandes <me@mdmfernandes.com>
2025-02-17 01:35:11 +0000

    yazi: update configs and add nvim plugin

---
+++ nvim/lua/plugins/yazi.lua
@@ -0,0 +1,32 @@
+-- yazi.nvim: https://github.com/mikavilpas/yazi.nvim
+return {
+    ---@type LazySpec
+    {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        keys = {
+            {
+                "<Leader>-",
+                "<Cmd>Yazi<CR>",
+                desc = "Open yazi at the current file location",
+            },
+            {
+                "<Leader>.",
+                "<Cmd>Yazi cwd<CR>",
+                desc = "Open yazi in nvim working directory",
+            },
+            {
+                "<C-.>",
+                "<Cmd>Yazi toggle<CR>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig
+        opts = {
+            open_for_directories = true,
+            keymaps = {
+                open_file_in_horizontal_split = "<c-h>",
+            },
+        },
+    }
+}
