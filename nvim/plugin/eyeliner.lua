c52893d36b8ee20fceb67338bbff1cb2df91c478
Miguel Fernandes <me@mdmfernandes.com>
2026-05-13 13:18:42 +0200

    nvim: set colors for the eyeliner plugin

--- nvim/plugin/eyeliner.lua
+++ nvim/plugin/eyeliner.lua
@@ -4,8 +4,9 @@ vim.pack.add({
     { src = "https://github.com/jinh0/eyeliner.nvim", name = "eyeliner" },
 })
 
-require("eyeliner").setup(
-    {
-        highlight_on_key = true,
-        -- dim = true, -- dim all other characters
-    })
+require("eyeliner").setup({
+    highlight_on_key = true,
+})
+
+vim.api.nvim_set_hl(0, "EyelinerPrimary", { fg = "#ffffff", bold = true })
+vim.api.nvim_set_hl(0, "EyelinerSecondary", { fg = "#f5bde6", bold = true })
